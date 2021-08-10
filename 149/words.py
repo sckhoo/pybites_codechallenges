a277aeece44e11f50ddf66bba8ce7e4203e96df6
sckhoo <sckhoo@gmail.com>
2021-08-10 11:53:11 +0800

    testing sorted key method

--- 149/words.py
+++ 149/words.py
@@ -1,6 +1,7 @@
-words = ("It was the twenty9th of October when it was questioned"
-             "the meaning of nuMbers and weather hiding a number Inside"
-             "tex56t should be treated as a word or another number").split()
+words = ("Andrew Carnegie's 64-room chateau at 2 East 91st "
+             "Street was converted into the Cooper-Hewitt National "
+             "Design Museum of the Smithsonian Institution "
+             "in the 1970's").split()
 
 def sort_words_case_insensitively(words):
     """Sort the provided word list ignoring case, and numbers last
@@ -8,14 +9,6 @@ def sort_words_case_insensitively(words):
         numbers you only need to check the first char of the word)
     """
     #temp = sorted(words, key=lambda test_str: test_str[:1].lower() + test_str[1:])
-    temp = sorted(words, key=str.lower)
-    temp1 = []
-    for index, word in enumerate(temp):
-        if not word[0].isdigit():
-            temp1.append(temp[index])
-    for index, word in enumerate(temp):
-        if word[0].isdigit():
-            temp1.append(temp[index])
-    return temp1
+    return sorted(words, key=lambda x: (x[0].isdigit(), str(x).lower()))
 
 print(sort_words_case_insensitively(words))
